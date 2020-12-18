a2865c43a69422d71e22cdae1599f7e40b9490c3
Blake Alford <balford120@gmail.com>
2020-12-18 23:26:32 +1100

    Update

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import basic
 
 while True:
-    text = input('basic > ')
-    result, error = basic.run('<stdin>', text)
+	text = input('basic > ')
+	result, error = basic.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error: print(error.as_string())
+	elif result: print(result)
